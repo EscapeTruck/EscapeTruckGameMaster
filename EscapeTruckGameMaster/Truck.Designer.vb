bb4a60223285dc2a99bf9f8b3edce00c14560dd2
Brandon <wolf.brandon@gmail.com>
2017-07-08 01:13:50 -0400

    Started Serial Read

--- EscapeTruckGameMaster/Truck.Designer.vb
+++ EscapeTruckGameMaster/Truck.Designer.vb
@@ -25,13 +25,13 @@ Partial Class Truck
         Me.cmboPuzzleCount = New System.Windows.Forms.ComboBox()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.cmboPlayerCount = New System.Windows.Forms.ComboBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.cmboDifficulty = New System.Windows.Forms.ComboBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmboDifficulty = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmboPlayerCount = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
@@ -40,9 +40,10 @@ Partial Class Truck
         '
         Me.cmboPuzzleCount.FormattingEnabled = True
         Me.cmboPuzzleCount.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.cmboPuzzleCount.Location = New System.Drawing.Point(6, 30)
+        Me.cmboPuzzleCount.Location = New System.Drawing.Point(4, 19)
+        Me.cmboPuzzleCount.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cmboPuzzleCount.Name = "cmboPuzzleCount"
-        Me.cmboPuzzleCount.Size = New System.Drawing.Size(121, 28)
+        Me.cmboPuzzleCount.Size = New System.Drawing.Size(82, 21)
         Me.cmboPuzzleCount.TabIndex = 0
         '
         'TabControl1
@@ -52,10 +53,11 @@ Partial Class Truck
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TabControl1.Controls.Add(Me.TabPage1)
         Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Location = New System.Drawing.Point(8, 8)
+        Me.TabControl1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(687, 420)
+        Me.TabControl1.Size = New System.Drawing.Size(458, 273)
         Me.TabControl1.TabIndex = 1
         '
         'TabPage1
@@ -67,87 +69,96 @@ Partial Class Truck
         Me.TabPage1.Controls.Add(Me.Label2)
         Me.TabPage1.Controls.Add(Me.Label1)
         Me.TabPage1.Controls.Add(Me.cmboPuzzleCount)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(679, 387)
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabPage1.Size = New System.Drawing.Size(450, 247)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Game Settings"
         Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1079, 500)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Truck Settings"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(7, 7)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(103, 20)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Puzzle Count"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(4, 65)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(99, 20)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Player Count"
-        '
-        'cmboPlayerCount
-        '
-        Me.cmboPlayerCount.FormattingEnabled = True
-        Me.cmboPlayerCount.Items.AddRange(New Object() {"1", "2", "3", "4"})
-        Me.cmboPlayerCount.Location = New System.Drawing.Point(4, 89)
-        Me.cmboPlayerCount.Name = "cmboPlayerCount"
-        Me.cmboPlayerCount.Size = New System.Drawing.Size(121, 28)
-        Me.cmboPlayerCount.TabIndex = 3
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(4, 124)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(69, 20)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Difficulty"
-        '
-        'cmboDifficulty
-        '
-        Me.cmboDifficulty.FormattingEnabled = True
-        Me.cmboDifficulty.Items.AddRange(New Object() {"Easy", "Medium", "Hard"})
-        Me.cmboDifficulty.Location = New System.Drawing.Point(4, 148)
-        Me.cmboDifficulty.Name = "cmboDifficulty"
-        Me.cmboDifficulty.Size = New System.Drawing.Size(121, 28)
-        Me.cmboDifficulty.TabIndex = 5
         '
         'Button1
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.AutoSize = True
-        Me.Button1.Location = New System.Drawing.Point(571, 351)
+        Me.Button1.Location = New System.Drawing.Point(379, 224)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(102, 30)
+        Me.Button1.Size = New System.Drawing.Size(70, 23)
         Me.Button1.TabIndex = 6
         Me.Button1.Text = "Start Game"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Main
+        'cmboDifficulty
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.cmboDifficulty.FormattingEnabled = True
+        Me.cmboDifficulty.Items.AddRange(New Object() {"Easy", "Medium", "Hard"})
+        Me.cmboDifficulty.Location = New System.Drawing.Point(3, 96)
+        Me.cmboDifficulty.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cmboDifficulty.Name = "cmboDifficulty"
+        Me.cmboDifficulty.Size = New System.Drawing.Size(82, 21)
+        Me.cmboDifficulty.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 81)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Difficulty"
+        '
+        'cmboPlayerCount
+        '
+        Me.cmboPlayerCount.FormattingEnabled = True
+        Me.cmboPlayerCount.Items.AddRange(New Object() {"1", "2", "3", "4"})
+        Me.cmboPlayerCount.Location = New System.Drawing.Point(3, 58)
+        Me.cmboPlayerCount.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cmboPlayerCount.Name = "cmboPlayerCount"
+        Me.cmboPlayerCount.Size = New System.Drawing.Size(82, 21)
+        Me.cmboPlayerCount.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 42)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Player Count"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 5)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Puzzle Count"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabPage2.Size = New System.Drawing.Size(450, 247)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Truck Settings"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Truck
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(711, 444)
+        Me.ClientSize = New System.Drawing.Size(474, 289)
         Me.Controls.Add(Me.TabControl1)
-        Me.Name = "Main"
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Truck"
         Me.Text = "Main"
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
--- EscapeTruckGameMaster/Truck.vb
+++ EscapeTruckGameMaster/Truck.vb
@@ -1,5 +1,41 @@
-﻿Public Class Truck
+﻿Imports System
+Imports System.IO.Ports
+
+Public Class Truck
+    Dim GameCom As IO.Ports.SerialPort = Nothing
+    Shared myGame As Game = Nothing
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        My.Forms.Game.Show()
+        myGame = New Game
+        myGame.Show()
+    End Sub
+
+    Private Sub Truck_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GameCom = My.Computer.Ports.OpenSerialPort("COM6")
+        With GameCom
+            .BaudRate = 9600
+            .Parity = Parity.None
+            .StopBits = StopBits.One
+            .DataBits = 8
+            .Handshake = Handshake.None
+            .RtsEnable = True
+
+            AddHandler .DataReceived, AddressOf DataReceivedHandler
+        End With
+        'GameCom.Open()
+    End Sub
+
+    Private Shared Sub DataReceivedHandler(
+                        sender As Object,
+                        e As SerialDataReceivedEventArgs)
+        Dim sp As SerialPort = CType(sender, SerialPort)
+        Dim indata As String = sp.ReadExisting()
+        Console.WriteLine("Data Received:")
+        Console.Write(indata)
+
+        If Not IsNothing(myGame) Then
+            myGame.txtInput.Text = indata
+            myGame.btnSubmitGuess.PerformClick()
+        End If
     End Sub
 End Class
